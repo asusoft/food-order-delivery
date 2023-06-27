74feb72041775bfc3c0fd8ff168b40074a74d188
Asusoft <asusoft19a@gmail.com>
2023-06-27 20:14:25 +0300

    define contant sizes and dimensions

--- user-app/assets/constants/theme.js
+++ user-app/assets/constants/theme.js
@@ -1,3 +1,5 @@
+import { Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('window');
 
 export const COLORS = {
     primary: '#ff9248',
@@ -14,5 +16,33 @@ export const COLORS = {
     green: '#00FF00',
     brown: '#5C4033'
   };
+
+
+  export const SIZES = {
+  // global sizes
+  base: 8,
+  font: 14,
+  radius: 12,
+  padding: 24,
+
+  // font sizes
+  largeTitle: 40,
+  h1: 30,
+  h2: 22,
+  h3: 16,
+  h4: 14,
+  h5: 12,
+  body1: 30,
+  body2: 22,
+  body3: 16,
+  body4: 14,
+  body5: 12,
+
+  // app dimensions
+  width,
+  height,
+};
   
-  export default COLORS;
+const appTheme = { COLORS, SIZES };
+
+export default appTheme;
