25e43361839eb4d42a1901c1f5016254eeb9eca7
Asusoft <asusoft19a@gmail.com>
2023-06-27 20:12:36 +0300

    define contants colors

---
+++ user-app/assets/constants/theme.js
@@ -0,0 +1,18 @@
+
+export const COLORS = {
+    primary: '#ff9248',
+    secondary: '#FFFAF0',
+    white: '#FFF',
+    dark: '#616161',
+    light: '#f5f5f5',
+    grey: '#808080',
+    lightGray: '#DDDDDD',
+    lightGray2: '#ededed',
+    background: '#FAF0E6',
+    transparentBlack: "rgba(0, 0, 0, 0.7)",
+    red: '#ff4545',
+    green: '#00FF00',
+    brown: '#5C4033'
+  };
+  
+  export default COLORS;
