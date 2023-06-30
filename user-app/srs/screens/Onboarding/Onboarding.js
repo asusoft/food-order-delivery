f9908e212f1249a06c93232a1779bc815da3008b
Asusoft <asusoft19a@gmail.com>
2023-06-30 13:50:18 +0300

    set navigation between onboarding and auth screens

--- user-app/srs/screens/Onboarding/Onboarding.js
+++ user-app/srs/screens/Onboarding/Onboarding.js
@@ -1,12 +1,29 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { COLORS } from '../../../assets/constants/theme';
+import { useNavigation } from '@react-navigation/native';
 
 // create a component
 const Onboarding = () => {
+
+    const navigation = useNavigation();
+
+    const onSignIn = () => {
+        navigation.navigate("Auth", { screen: 'SignIn' })
+    }
+    const onSignUp = () => {
+        navigation.navigate("Auth", { screen: 'SignUp' })
+    }
     return (
         <View style={styles.container}>
             <Text>Onboarding</Text>
+            <TouchableOpacity onPress={() => onSignIn()}>
+                <Text> Login </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginTop: 20 }} onPress={() => onSignUp()}>
+                <Text> Create an account </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -17,7 +34,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        backgroundColor: COLORS.primary,
     },
 });
 
