bb7ce70831d8b27829344df206fe98bb314cee61
Asusoft <asusoft19a@gmail.com>
2023-07-02 03:13:00 +0300

    create home screen

---
+++ user-app/srs/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,26 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { COLORS } from '../../../assets/constants/theme';
+
+// create a component
+const HomeScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.background,
+    },
+});
+
+//make this component available to the app
+export default HomeScreen;
