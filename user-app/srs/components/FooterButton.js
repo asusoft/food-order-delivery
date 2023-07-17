93d2f9c5bef97ca0cf6f8c40b6cd3319fd866bfb
Asusoft <asusoft19a@gmail.com>
2023-07-17 04:36:43 +0300

    Revert "Merge pull request #6 from asusoft/browse-merchant"
    This reverts commit e7f4379f48e1fd35c5126d9733c51bca11d3ec2e, reversing
    changes made to 496e7b3fbfddc2e478891b56d16137c6a827b8fa.

--- user-app/srs/components/FooterButton.js
+++ user-app/srs/components/FooterButton.js
@@ -8,9 +8,7 @@ const FooterButton = ({
     label,
     onPress,
     disabled,
-    footerStyle,
-    rightComponent,
-    leftComponent
+    footerStyle
 }) => {
     return (
         <TouchableOpacity
@@ -19,11 +17,9 @@ const FooterButton = ({
             opacity={0.5}
             style={{ ...styles.Footer, opacity: disabled ? 0.5 : 1, ...footerStyle, }}
         >
-            {leftComponent}
             <Text style={{ fontSize: 20, color: COLORS.light, fontWeight: "600" }}>
                 {label}
             </Text>
-            {rightComponent}
         </TouchableOpacity>
     );
 };
